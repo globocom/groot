7a8fac618296c046c797fe20a34422251ca49a3b
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-09-22 01:13:05 -0300

    Statsd initial support

--- src/main/java/com/globocom/grou/groot/loader/LoaderService.java
+++ src/main/java/com/globocom/grou/groot/loader/LoaderService.java
@@ -8,6 +8,7 @@ import com.globocom.grou.groot.jbender.JBender;
 import com.globocom.grou.groot.jbender.events.TimingEvent;
 import com.globocom.grou.groot.jbender.executors.Validator;
 import com.globocom.grou.groot.jbender.executors.http.FiberApacheHttpClientRequestExecutor;
+import io.galeb.statsd.NonBlockingStatsDClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -15,20 +16,24 @@ import org.apache.http.client.methods.HttpGet;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
+@SuppressWarnings({"unchecked", "Convert2MethodRef"})
 @Service
-@SuppressWarnings({"OptionalUsedAsFieldOrParameterType", "ConstantConditions"})
 public class LoaderService {
 
     private static final String GROOT_USERAGENT = "Grou$Groot/1.0";
+    private static final String STATSD_PREFIX = Optional.ofNullable(System.getenv("STATSD_PREFIX")).orElse("groot");
+    private static final String STATSD_HOST   = Optional.ofNullable(System.getenv("STATSD_HOST")).orElse("localhost");
+    private static final int    STATSD_PORT   = Integer.parseInt(Optional.ofNullable(System.getenv("STATSD_PORT")).orElse("8125"));
 
     private final Log log = LogFactory.getLog(this.getClass());
 
+    private final NonBlockingStatsDClient statsDClient = new NonBlockingStatsDClient(STATSD_PREFIX, STATSD_HOST, STATSD_PORT);
+
     public void start(String testName, String uri, int numConn, int durationTimeMillis) throws IOException, ExecutionException, InterruptedException {
-        final Validator<CloseableHttpResponse> responseValidator = response -> {
-            sendToStatsd(response);
-        };
+        final Validator<CloseableHttpResponse> responseValidator = response -> sendToStatsd(testName, response);
         try (final FiberApacheHttpClientRequestExecutor requestExecutor = new FiberApacheHttpClientRequestExecutor<>(responseValidator, 1000000)) {
 
             final Channel<HttpGet> requestChannel = Channels.newChannel(10000, Channels.OverflowPolicy.DROP);
@@ -50,7 +55,8 @@ public class LoaderService {
         log.info("Finished test " + testName);
     }
 
-    private void sendToStatsd(CloseableHttpResponse response) {
-
+    private void sendToStatsd(String testName, CloseableHttpResponse response) {
+        int statusCode = response.getStatusLine().getStatusCode();
+        statsDClient.incrementCounter(testName + ".httpStatus" + statusCode);
     }
 }
