e4e12a3dc155265b4a82dcfe312cdc878e34724c
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-09-27 07:49:08 -0300

    Using AHC directly

---
+++ src/main/java/com/globocom/grou/groot/httpclient/RequestExecutorService.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2017-2017 Globo.com
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globocom.grou.groot.httpclient;
+
+import com.globocom.grou.groot.statsd.StatsdService;
+import io.galeb.statsd.StatsDClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.asynchttpclient.AsyncCompletionHandler;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Response;
+import org.asynchttpclient.exception.TooManyConnectionsException;
+import org.asynchttpclient.exception.TooManyConnectionsPerHostException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RequestExecutorService {
+
+    private final Log log = LogFactory.getLog(this.getClass());
+
+    private final StatsDClient statsdClient;
+
+    @Autowired
+    public RequestExecutorService(StatsdService statsdService) {
+        this.statsdClient = statsdService.client();
+    }
+
+    public void execute(final AsyncHttpClient asyncHttpClient, final ParameterizedRequest request) {
+
+        String prefixStatsdKey = request.getTestProject() + "." + request.getTestName();
+        final long start = System.currentTimeMillis();
+        asyncHttpClient.executeRequest(request, new AsyncCompletionHandler<Response>() {
+
+            @Override
+            public Response onCompleted(Response response) throws Exception {
+                int statusCode = response.getStatusCode();
+                statsdClient.recordExecutionTime(prefixStatsdKey + "." + statusCode, System.currentTimeMillis() - start);
+                return response;
+            }
+
+            @Override
+            public void onThrowable(Throwable t) {
+                log.error(t);
+                if (!((t instanceof TooManyConnectionsException) || (t instanceof TooManyConnectionsPerHostException))) {
+                    String messageException = t.getMessage().replaceAll("[ .:/]", "_").replaceAll(".*Exception__", "");
+                    statsdClient.recordExecutionTime(prefixStatsdKey + "." + messageException, System.currentTimeMillis() - start);
+                }
+            }
+        });
+    }
+}
--- src/main/java/com/globocom/grou/groot/loader/LoaderService.java
+++ src/main/java/com/globocom/grou/groot/loader/LoaderService.java
@@ -20,31 +20,34 @@ import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.strands.SuspendableRunnable;
 import co.paralleluniverse.strands.channels.Channel;
 import co.paralleluniverse.strands.channels.Channels;
+import com.globocom.grou.groot.Application;
 import com.globocom.grou.groot.entities.Test;
-import com.globocom.grou.groot.jbender.JBender;
-import com.globocom.grou.groot.jbender.events.TimingEvent;
-import com.globocom.grou.groot.jbender.executors.http.FiberAHC2RequestExecutor;
-import com.globocom.grou.groot.jbender.util.AHC2ParameterizedRequest;
+import com.globocom.grou.groot.httpclient.RequestExecutorService;
+import com.globocom.grou.groot.httpclient.ParameterizedRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.asynchttpclient.Response;
+import org.asynchttpclient.AsyncHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
+
+import static org.asynchttpclient.Dsl.asyncHttpClient;
+import static org.asynchttpclient.Dsl.config;
 
 @SuppressWarnings({"unchecked", "Convert2MethodRef"})
 @Service
 public class LoaderService {
 
+    private final RequestExecutorService asyncHttpClientService;
+
     private final Log log = LogFactory.getLog(this.getClass());
 
-    private final JBender jBender;
-
     @Autowired
-    public LoaderService(JBender jBender) {
-        this.jBender = jBender;
+    public LoaderService(RequestExecutorService asyncHttpClientService) {
+        this.asyncHttpClientService = asyncHttpClientService;
     }
 
     public void start(Test test, final Map<String, Object> properties) throws Exception {
@@ -52,32 +55,57 @@ public class LoaderService {
         final int numConn = Optional.ofNullable((Integer) properties.get("numConn")).orElseThrow(() -> new IllegalArgumentException("numConn property undefined"));
         final int durationTimeMillis = Optional.ofNullable((Integer) properties.get("durationTimeMillis")).orElseThrow(() -> new IllegalArgumentException("durationTimeMillis property undefined"));
 
-        final AHC2ParameterizedRequest requestBuilder = new AHC2ParameterizedRequest(test);
+        final ParameterizedRequest requestBuilder = new ParameterizedRequest(test);
+        final Channel<ParameterizedRequest> requestChannel = Channels.newChannel(10000, Channels.OverflowPolicy.DROP);
 
-        try (final FiberAHC2RequestExecutor requestExecutor = new FiberAHC2RequestExecutor(numConn)) {
+        log.info("Starting test " + testName);
+        final long start = System.currentTimeMillis();
 
-            final Channel<AHC2ParameterizedRequest> requestChannel = Channels.newChannel(10000, Channels.OverflowPolicy.DROP);
-            final Channel<TimingEvent<Response>> eventChannel = Channels.newChannel(10000, Channels.OverflowPolicy.DROP);
-            final long start = System.currentTimeMillis();
-            log.info("Starting test " + testName);
-            final SuspendableRunnable requestGeneratorRunner = () -> {
-                try {
-                    while (System.currentTimeMillis() - start < durationTimeMillis) {
-                        requestChannel.send(requestBuilder);
-                    }
-                } catch (Exception e) {
-                    log.error(e.getMessage(), e);
-                } finally {
-                    log.warn("closing " + requestChannel);
-                    requestChannel.close();
+        final SuspendableRunnable requestGeneratorRunner = () -> {
+            try {
+                while (System.currentTimeMillis() - start < durationTimeMillis) {
+                    requestChannel.send(requestBuilder);
                 }
-            };
-            final SuspendableRunnable jbenderRunner = () -> jBender.loadTestConcurrency(numConn, 0, requestChannel, requestExecutor, eventChannel);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            } finally {
+                log.warn("closing " + requestChannel);
+                requestChannel.close();
+            }
+        };
 
-            new Fiber<Void>("request-generator", requestGeneratorRunner).start();
-            new Fiber<Void>("jbender", jbenderRunner).start().join();
-        }
+        final SuspendableRunnable requestExecutor = () -> {
+            try (final AsyncHttpClient asyncHttpClient = newAsyncHttpClient(numConn, durationTimeMillis)) {
+                while (true) {
+                    final ParameterizedRequest request = requestChannel.receive();
+                    if (request == null) {
+                        break;
+                    }
+                    asyncHttpClientService.execute(asyncHttpClient, request);
+                }
+            } catch (IOException e) {
+                log.error(e);
+            }
+        };
+
+        new Fiber<Void>("request-generator", requestGeneratorRunner).start();
+        new Fiber<Void>("request-executor", requestExecutor).start().join();
+
         log.info("Finished test " + testName);
+    }
+
+    private AsyncHttpClient newAsyncHttpClient(int numConn, int durationTimeMillis) {
+        return asyncHttpClient(config()
+                    .setFollowRedirect(false)
+                    .setSoReuseAddress(true)
+                    .setKeepAlive(true)
+                    .setConnectTimeout(2000)
+                    .setPooledConnectionIdleTimeout(durationTimeMillis)
+                    .setConnectionTtl(durationTimeMillis)
+                    .setMaxConnectionsPerHost(numConn)
+                    .setMaxConnections(numConn)
+                    .setUseInsecureTrustManager(true)
+                    .setUserAgent(Application.GROOT_USERAGENT).build());
     }
 
 }
