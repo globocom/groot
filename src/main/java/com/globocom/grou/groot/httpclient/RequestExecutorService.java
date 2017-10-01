4b60fc8f529ff8ed47211c84dfec27ad5c6ae9d5
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-10-01 12:40:06 -0300

    Move statsd dependency to MonitorService

--- src/main/java/com/globocom/grou/groot/httpclient/RequestExecutorService.java
+++ src/main/java/com/globocom/grou/groot/httpclient/RequestExecutorService.java
@@ -17,17 +17,14 @@
 package com.globocom.grou.groot.httpclient;
 
 import com.globocom.grou.groot.Application;
-import com.globocom.grou.groot.statsd.StatsdService;
+import com.globocom.grou.groot.statsd.MonitorService;
 import com.globocom.grou.groot.statsd.SystemInfo;
-import io.galeb.statsd.StatsDClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.asynchttpclient.AsyncCompletionHandler;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
 import org.asynchttpclient.Response;
-import org.asynchttpclient.exception.TooManyConnectionsException;
-import org.asynchttpclient.exception.TooManyConnectionsPerHostException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -42,35 +39,31 @@ public class RequestExecutorService {
 
     private final Log log = LogFactory.getLog(this.getClass());
 
-    private final StatsDClient statsdClient;
+    private final MonitorService monitorService;
 
     @Autowired
-    public RequestExecutorService(StatsdService statsdService) {
-        this.statsdClient = statsdService.client();
+    public RequestExecutorService(MonitorService monitorService) {
+        this.monitorService = monitorService;
     }
 
     public void execute(final AsyncHttpClient asyncHttpClient, final ParameterizedRequest request) {
 
-        String prefixStatsdKey = request.getTestProject() + "." + request.getTestName();
         final long start = System.currentTimeMillis();
         asyncHttpClient.executeRequest(request, new AsyncCompletionHandler<Response>() {
 
             @Override
             public Response onCompleted(Response response) throws Exception {
-                int statusCode = response.getStatusCode();
-                int bodySize = response.getResponseBodyAsBytes().length;
-                statsdClient.recordExecutionTime(prefixStatsdKey + ".status." + statusCode, System.currentTimeMillis() - start);
-                statsdClient.count(prefixStatsdKey + ".responseSize", bodySize);
+                try {
+                    monitorService.completed(response, start);
+                } catch (Exception e) {
+                    onThrowable(e);
+                }
                 return response;
             }
 
             @Override
             public void onThrowable(Throwable t) {
-                if (!((t instanceof TooManyConnectionsException) || (t instanceof TooManyConnectionsPerHostException) || t.getMessage().contains("executor not accepting a task"))) {
-                    String messageException = t.getMessage().replaceAll("[ .:/]", "_").replaceAll(".*Exception__", "");
-                    statsdClient.recordExecutionTime(prefixStatsdKey + "." + messageException, System.currentTimeMillis() - start);
-                    log.error(t);
-                }
+                monitorService.fail(t, start);
             }
         });
     }
