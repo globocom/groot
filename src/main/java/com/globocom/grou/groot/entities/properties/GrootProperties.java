b8b37fc9c83319adfb863629fc92bb1a8024ac3f
Joao Machado Borsani (jmborsani) <joao.borsani@gmail.com>
2017-10-27 15:55:14 -0200

    Add GrootProperties

---
+++ src/main/java/com/globocom/grou/groot/entities/properties/GrootProperties.java
@@ -0,0 +1,23 @@
+package com.globocom.grou.groot.entities.properties;
+
+public interface GrootProperties {
+
+    /**
+     * Authentication Basic
+     */
+    String AUTH                 = "auth";
+    String BODY                 = "body";
+    String CREDENTIALS          = "credentials";
+    String HEADERS              = "headers";
+    String METHOD               = "method";
+    String PREEMPTIVE           = "preemptive";
+    String URI                  = "uri";
+    String PARALLEL_LOADERS     = "parallelLoaders";
+    String CONNECTION_TIMEOUT   = "connectTimeout";
+    String KEEP_ALIVE           = "keepAlive";
+    String FOLLOW_REDIRECT      = "followRedirect";
+    String NUM_CONN             = "numConn";
+    String DURATION_TIME_MILLIS = "durationTimeMillis";
+    String FIXED_DELAY          = "fixedDelay";
+    String MONIT_TARGETS        = "monitTargets";
+}
