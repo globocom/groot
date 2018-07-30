d4d05e3375fa60073ac35025e27d374d70aaa3f9
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-07-30 15:50:44 -0300

    contrib/nghttpd: improve script

--- contrib/nghttpd/run.sh
+++ contrib/nghttpd/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-nghttpd $1 -d . --no-content-length -n $[($(nproc)/4)+1] 8445 ./server.key ./server.crt
+if [ "x$(uname)" == "xDarwin" ]; then
+  numcores="$(sysctl -n machdep.cpu.core_count)"
+else
+  numcores="$(nproc)"
+fi
+
+nghttpd $1 -d . --no-content-length -n $[($numcores/4)+1] 8445 ./server.key ./server.crt
