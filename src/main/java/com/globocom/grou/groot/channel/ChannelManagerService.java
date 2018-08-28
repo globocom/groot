8b779c30e0545e96c8027de3f0373c7a5c135659
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-08-28 08:17:53 -0300

    Add ChannelManagerService

---
+++ src/main/java/com/globocom/grou/groot/channel/ChannelManagerService.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2017-2018 Globo.com
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globocom.grou.groot.channel;
+
+import com.globocom.grou.groot.channel.handler.Http1ClientInitializer;
+import com.globocom.grou.groot.channel.handler.Http2ClientInitializer;
+import com.globocom.grou.groot.loader.Proto;
+import com.globocom.grou.groot.monit.MonitorService;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChannelManagerService {
+
+    private static final Log LOGGER = LogFactory.getLog(ChannelManagerService.class);
+
+    private final SslService sslService;
+    private final MonitorService monitorService;
+
+    private long schedPeriod = 50L;
+
+    @Autowired
+    public ChannelManagerService(SslService sslService, MonitorService monitorService) {
+        this.sslService = sslService;
+        this.monitorService = monitorService;
+    }
+
+    private ChannelInitializer initializer(Proto proto) {
+        if (proto == Proto.H2 || proto == Proto.H2C) {
+            return new Http2ClientInitializer(sslService.sslContext(proto.isSsl()), Integer.MAX_VALUE, monitorService);
+        }
+        return new Http1ClientInitializer(sslService.sslContext(proto.isSsl()), monitorService);
+    }
+
+    private Channel newChannel(final Bootstrap bootstrap, Proto proto, final FullHttpRequest[] requests, long schedPeriod) {
+        try {
+            if (!bootstrap.config().group().isShuttingDown() && !bootstrap.config().group().isShutdown()) {
+                URI uri = URI.create(proto.name().toLowerCase() + "://" + requests[0].headers().get(HttpHeaderNames.HOST) + requests[0].uri());
+                final Channel channel = bootstrap
+                    .clone()
+                    .handler(initializer(proto))
+                    .connect(uri.getHost(), uri.getPort())
+                    .sync()
+                    .channel();
+                channel.eventLoop().scheduleAtFixedRate(() -> {
+                    if (channel.isActive()) {
+                        for (FullHttpRequest request : requests) {
+                            monitorService.writeCounterIncr();
+                            channel.writeAndFlush(request.copy());
+                        }
+                    }
+                }, schedPeriod, schedPeriod, TimeUnit.MICROSECONDS);
+                return channel;
+            }
+        } catch (Exception e) {
+            monitorService.fail(e);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(e.getMessage(), e);
+            }
+        }
+        return null;
+    }
+
+    public synchronized void activeChannels(int numConn, final Proto proto, final Bootstrap bootstrap, final Channel[] channels, final FullHttpRequest[] requests) {
+        double lastPerformanceRate = monitorService.lastPerformanceRate();
+        schedPeriod = Math.min(100, Math.max(10L, (long) (schedPeriod * lastPerformanceRate / 1.05)));
+        LOGGER.info("Sched Period: " + schedPeriod + " us");
+
+        for (int chanId = 0; chanId < numConn; chanId++) {
+            if (channels[chanId] == null || !channels[chanId].isActive()) {
+                Channel channel = newChannel(bootstrap, proto, requests, schedPeriod);
+                if (channel != null) {
+                    channels[chanId] = channel;
+                }
+            }
+        }
+    }
+
+    public void reconnectIfNecessary(boolean reconnect, int numConn, final Proto proto, final EventLoopGroup group, Bootstrap bootstrap, Channel[] channels, final FullHttpRequest[] requests) {
+        while (reconnect && !group.isShutdown() && !group.isShuttingDown()) {
+            activeChannels(numConn, proto, bootstrap, channels, requests);
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                // ignored
+            }
+        }
+    }
+
+    public void closeChannels(Channel[] channels, int timeout, TimeUnit unit) {
+        CountDownLatch latch = new CountDownLatch(channels.length - 1);
+        for (Channel channel : channels) {
+            try {
+                if (channel != null && channel.isActive()) {
+                    channel.close();
+                }
+            } finally {
+                latch.countDown();
+            }
+        }
+        try {
+            latch.await(timeout, unit);
+        } catch (InterruptedException e) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(e.getMessage(), e);
+            }
+        }
+    }
+
+}
--- src/main/java/com/globocom/grou/groot/loader/RequestExecutorService.java
+++ src/main/java/com/globocom/grou/groot/loader/RequestExecutorService.java
@@ -2,22 +2,16 @@ package com.globocom.grou.groot.loader;
 
 import com.globocom.grou.groot.SystemEnv;
 import com.globocom.grou.groot.channel.BootstrapBuilder;
+import com.globocom.grou.groot.channel.ChannelManagerService;
 import com.globocom.grou.groot.channel.RequestUtils;
 import com.globocom.grou.groot.channel.handler.CookieStorageHandler;
-import com.globocom.grou.groot.channel.handler.Http1ClientInitializer;
-import com.globocom.grou.groot.channel.handler.Http2ClientInitializer;
-import com.globocom.grou.groot.channel.SslService;
 import com.globocom.grou.groot.monit.MonitorService;
 import com.globocom.grou.groot.test.properties.BaseProperty;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpHeaderNames;
-import java.net.URI;
 import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
@@ -31,99 +25,18 @@ public class RequestExecutorService {
 
     private static final Log LOGGER = LogFactory.getLog(RequestExecutorService.class);
 
-    private final SslService sslService;
+    private final ChannelManagerService channelManagerService;
     private final MonitorService monitorService;
 
     private final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
 
-    private long schedPeriod = 50L;
-
     @Autowired
     public RequestExecutorService(
-        final SslService sslService,
+        final ChannelManagerService channelManagerService,
         final MonitorService monitorService) {
 
-        this.sslService = sslService;
+        this.channelManagerService = channelManagerService;
         this.monitorService = monitorService;
-    }
-
-    private ChannelInitializer initializer(Proto proto) {
-        if (proto == Proto.H2 || proto == Proto.H2C) {
-            return new Http2ClientInitializer(sslService.sslContext(proto.isSsl()), Integer.MAX_VALUE, monitorService);
-        }
-        return new Http1ClientInitializer(sslService.sslContext(proto.isSsl()), monitorService);
-    }
-
-    private Channel newChannel(final Bootstrap bootstrap, Proto proto, final FullHttpRequest[] requests, long schedPeriod) {
-        try {
-            if (!bootstrap.config().group().isShuttingDown() && !bootstrap.config().group().isShutdown()) {
-                URI uri = URI.create(proto.name().toLowerCase() + "://" + requests[0].headers().get(HttpHeaderNames.HOST) + requests[0].uri());
-                final Channel channel = bootstrap
-                    .clone()
-                    .handler(initializer(proto))
-                    .connect(uri.getHost(), uri.getPort())
-                    .sync()
-                    .channel();
-                channel.eventLoop().scheduleAtFixedRate(() -> {
-                    if (channel.isActive()) {
-                        for (FullHttpRequest request : requests) {
-                            monitorService.writeCounterIncr();
-                            channel.writeAndFlush(request.copy());
-                        }
-                    }
-                }, schedPeriod, schedPeriod, TimeUnit.MICROSECONDS);
-                return channel;
-            }
-        } catch (Exception e) {
-            monitorService.fail(e);
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug(e.getMessage(), e);
-            }
-        }
-        return null;
-    }
-
-    private synchronized void activeChannels(int numConn, final Proto proto, final Bootstrap bootstrap, final Channel[] channels, final FullHttpRequest[] requests, long schedPeriod) {
-        for (int chanId = 0; chanId < numConn; chanId++) {
-            if (channels[chanId] == null || !channels[chanId].isActive()) {
-
-                Channel channel = newChannel(bootstrap, proto, requests, schedPeriod);
-                if (channel != null) {
-                    channels[chanId] = channel;
-                }
-            }
-        }
-    }
-
-    private void reconnectIfNecessary(boolean reconnect, int numConn, final Proto proto, final EventLoopGroup group, Bootstrap bootstrap, Channel[] channels, final FullHttpRequest[] requests, long schedPeriod) {
-        while (reconnect && !group.isShutdown() && !group.isShuttingDown()) {
-            activeChannels(numConn, proto, bootstrap, channels, requests, schedPeriod);
-            try {
-                TimeUnit.MILLISECONDS.sleep(100);
-            } catch (InterruptedException e) {
-                // ignored
-            }
-        }
-    }
-
-    private void closeChannels(Channel[] channels, int timeout, TimeUnit unit) {
-        CountDownLatch latch = new CountDownLatch(channels.length - 1);
-        for (Channel channel : channels) {
-            try {
-                if (channel != null && channel.isActive()) {
-                    channel.close();
-                }
-            } finally {
-                latch.countDown();
-            }
-        }
-        try {
-            latch.await(timeout, unit);
-        } catch (InterruptedException e) {
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug(e.getMessage(), e);
-            }
-        }
     }
 
     public void submit(BaseProperty property) throws RuntimeException {
@@ -145,17 +58,12 @@ public class RequestExecutorService {
         final EventLoopGroup group = bootstrap.config().group();
 
         Channel[] channels = new Channel[numConn];
-        double lastPerformanceRate = monitorService.lastPerformanceRate();
-        schedPeriod = Math.min(100, Math.max(10L, (long) (schedPeriod * lastPerformanceRate / 1.05)));
-
-        LOGGER.info("Sched Period: " + schedPeriod + " us");
-
-        activeChannels(numConn, proto, bootstrap, channels, requests, schedPeriod);
+        channelManagerService.activeChannels(numConn, proto, bootstrap, channels, requests);
 
         executor.schedule(() -> {
             long nowPreShut = System.currentTimeMillis();
 
-            closeChannels(channels, 10, TimeUnit.SECONDS);
+            channelManagerService.closeChannels(channels, 10, TimeUnit.SECONDS);
             group.shutdownGracefully(1L, 10L, TimeUnit.SECONDS);
 
             monitorService.showReport((System.currentTimeMillis() - nowPreShut));
@@ -163,7 +71,7 @@ public class RequestExecutorService {
         }, durationSec, TimeUnit.SECONDS);
 
         boolean forceReconnect = property.getForceReconnect();
-        reconnectIfNecessary(forceReconnect, numConn, proto, group, bootstrap, channels, requests, schedPeriod);
+        channelManagerService.reconnectIfNecessary(forceReconnect, numConn, proto, group, bootstrap, channels, requests);
     }
 
 }
